de9672aafea4df048cdee026396596c5b24f5e64
Alessandro <as-web@hotmail.com>
2020-03-09 11:11:59 +0000

    search styled

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 class Model {
   constructor() {
     this.URL = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4b04e5a207a38d712ac2460337479c38&language=en-US&page=1';
@@ -5,8 +6,17 @@ class Model {
     this.movies = []
   }
 
+  bindMoviesListChanged(callback) {
+    this.onBindMoviesListChanged = callback;
+  }
+
   updateSearchText(ev) {
     this.searchText = ev.target.value
+  }
+
+  sortMoviesBy(sortBy) {
+    this.movies.sort((a, b) => b[sortBy] - a[sortBy])
+    this.onBindMoviesListChanged(this.movies)
   }
 
   handleSubmit = (evt) => {
@@ -14,9 +24,15 @@ class Model {
 
     fetch(this.URL)
       .then((response) => response.json())
-      .then(data => console.log(data))
+      .then(data => {
+        this.movies = data.results
+        this.sortMoviesBy('vote_average')
 
-    this.movies = ['movie1', 'movie2']
+
+        this.onBindMoviesListChanged(data)
+      })
+
+    // this.movies = ['movie1', 'movie2']
   }
 
 }
@@ -25,7 +41,8 @@ class View {
   constructor() {
     this.resultsHTML = document.getElementById("results")
     this.searchInput = document.getElementById("search_input");
-    this.submitButton = document.getElementById("submit_button");
+    this.submitButton = document.getElementById("submit_trigger");
+    this.sortyByVoteButton = document.getElementById("sort_by_vote")
   }
 
   handleOnChange() {
@@ -33,14 +50,24 @@ class View {
   }
 
   bindSubmit = handler => {
-    this.submitButton.addEventListener('click', e => { handler(e) })
+    this.submitButton.addEventListener('click', e => handler(e))
+  }
+
+  bindSortByVote = handler => {
+    this.sortyByVoteButton.addEventListener('click', () => handler())
   }
 
   setMovieHTML(movie) {
-    return `<li>${movie}</li>`
+    const { overview, title } = movie
+
+    return `
+      <li>
+        <span class="info">${title}</span>
+        <span class="info">${overview}</span>
+      </li>`
   }
 
-  render() {
+  render(results) {
     this.resultsHTML.innerHTML = results.map(movie => this.setMovieHTML(movie))
   }
 }
@@ -50,10 +77,16 @@ class Controller {
     this.model = model
     this.view = view
 
+    this.model.bindMoviesListChanged(this.onBindMoviesListChanged)
     this.view.bindSubmit(this.handleSubmit)
+    this.view.bindSortByVote(this.handleBySortByVote)
   }
 
   handleSubmit = e => this.model.handleSubmit(e)
+
+  onBindMoviesListChanged = () => this.view.render(this.model.movies)
+
+  handleBySortByVote = () => this.model.sortMoviesBy('vote_count')
 }
 
 const App = new Controller(new Model(), new View())
